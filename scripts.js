a3b63cace3b63e1ae4d99f11b870ad4344941a9e
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2024-12-05 21:20:18 +0100

    elagazas bevezetese

--- scripts.js
+++ scripts.js
@@ -2,36 +2,31 @@ const array = [
     { // az array tömb első elemének létrehozása
         Uralkodo_name: `I. István`,//értéket adunk az array tömb első elemének Uralkodo_name tulajdonságának
         esemeny: `Koronázás`,//értéket adunk az array tömb első elemének esemeny tulajdonságának
-        evszam: `1000`//értéket adunk az array tömb első elemének evszam tulajdonságának   
+        evszam: `1000`,//értéket adunk az array tömb első elemének evszam tulajdonságának  
+        esemeny2: `Pannonhalmi apátság megalapítása`, //értéket adunk a tömbünk első elemének esemeny2 tulajdonságának
+        evszam2: `1001`//értéket adunk a tömbünk első elemének evszam2 tulajdonságának 
     },
-    {// az array tömb második elemének deklarációja
-        esemeny: `Pannonhalmi apátság megalapítása`, //értéket adunk a tömbünk második elemének esemeny2 tulajdonságának
-        evszam: `1001`//értéket adunk a tömbünk második elemének evszam2 tulajdonságának
-    },
-    {// az array tömb harmadik elemének létrehozása
+    {// az array tömb második elemének létrehozása
         Uralkodo_name: `IV. Béla`,// array tömb harmadik elemének Uralkodo_name tulajdonságának értéket adunk
         esemeny: `tatárjárás`,//az array tömbünk harmadik elemének esemeny tulajdonságának értéket adunk
         evszam: `1241-1242`// a tömbünk harmadik elemének evszam tulajdonságához értéket adunk
     },
-    {// az array tömb negyedik elemének deklarációja
+    {// az array tömb harmadik elemének deklarációja
         Uralkodo_name: `Mátyás király`,// a tömbünk negyedik elemének Uralkodo_name tulajdonságához értéket rendelek
         esemeny: `Bécs elfoglalása`,// az arrayem negyedik elemének esemeny tulajdonságához értéket adok
-        evszam: `1485`// array tömb negyedik elemének evszam tulajdonságának értékadás
+        evszam: `1485`,// array tömb negyedik elemének evszam tulajdonságának értékadás
+        esemeny2: `Kenyérmezei csata`, //az array nevű tömböm 4. elemének esemeny2 tulajdonságának értéket adok
+        evszam2: `1479`//a tömböm 4. elemének evszam2 tulajdonságának értéket adunk
     },
-    {// az array tömb 5. elemének deklarációja
-        esemeny: `Kenyérmezei csata`, //az array nevű tömböm 5. elemének esemeny2 tulajdonságának értéket adok
-        evszam: `1479`//a tömböm 5. elemének evszam2 tulajdonságának értéket adunk
-    },
-    {// az array tömb hatodik elemének deklarációja
+    {// az array tömb negyedik elemének deklarációja
         Uralkodo_name: `II. Rákóczi Ferenc`, // a tömböm hatodik elemének Uralkodo_name tulajdonságához értéket rendelek
         esemeny: `A szabadságharc kezdete`,// az array-em hatodik elemének esemeny tulajdonságának értéket adok
-        evszam: `1703`,
-        // az array nevű tömböm hatodik tagjának evszam tulajdonságának értékadás
+        evszam: `1703`,//az array nevű tömböm hatodik tagjának evszam tulajdonságának értékadás
+        esemeny2: `A szabadságharc vége`,// a tömböm 6.(utolsó) elemének esemeny2 tulajdonságának adok értéket
+        evszam2: `1711` // az array-em 6. tagjának evszam2_rakoczi tulajdonságának értéket adok
     },
-    {// az array tömb heteddik elemének deklarációja
-        esemeny: `A szabadságharc vége`,// a tömböm 7.(utolsó) elemének esemeny2 tulajdonságának adok értéket
-        evszam: `1711` // az array-em 7. tagjának evszam2_rakoczi tulajdonságának értéket adok
-    }
+
+
 ];
 
 
@@ -84,9 +79,7 @@ fejrow.appendChild(fejcella3);//hozzáfűzöm ezt a th elemet a fejléc sorához
 fejcella3.innerHTML = fejcellak.fejcella3label; // a cella tartalma a fejcellak objektum  fejcella3label tulajdonságának értéke lesz
 
 
-for(let a = 0; a < array.length; a++){//végig iterálunk a tömbön egy növekményes for ciklussal
-
-    const jelenElem  = array[a]; //deklarálok egy block szintű változót, aminek értékül adom az array jelenlegi ciklusváltozó számú elemet
+for(const jelenElem of array){//végig iterálunk a tömbön egy növekményes for ciklussal
     //a táblázat törzsének sorai
     const sor = document.createElement(`tr`);//létrehozok egy tableRow elemet
     tablatest.appendChild(sor);//Hozzáfűzöm ezt a sort a táblázatom törzséhez.
@@ -95,13 +88,39 @@ for(let a = 0; a < array.length; a++){//végig iterálunk a tömbön egy növekm
     sorUralkodoName.innerHTML  = jelenElem.Uralkodo_name;// a cella tartalma a jelenlegi elem Uralkodo_name tulajdonságának értéke lesz
     sor.appendChild(sorUralkodoName);//Hozzáfűzöm a most deklarált td elemet a fentebb létrehozott sorhoz
 
-
     const sorEsemeny  = document.createElement(`td`); //létrehozok ismét egy td elemet
     sorEsemeny.innerHTML = jelenElem.esemeny;// a cella tartalma az éppen aktuális elem esemeny tulajdonságának értéke lesz
     sor.appendChild(sorEsemeny);// Hozzáfűzöm ezt az elemet a korábban létrehozott soromhoz.
 
-
+    
     const  sorEvszam  = document.createElement(`td`);
     sorEvszam.innerHTML = jelenElem.evszam;// a cella tartalma az éppen aktuális elem evszam tulajdonsága lesz
     sor.appendChild(sorEvszam);// Rögtön hozzáfűzöm ezt az elemet a sorhoz.
+
+    if(jelenElem.esemeny2 !== undefined && jelenElem.evszam2 !== undefined){
+        sorUralkodoName.rowSpan = 2;
+        const sor2 = document.createElement(`tr`); // létrehozok még egy tableRow elementet a második eseménynek és évszámnak
+        tablatest.appendChild(sor2);//hozzáfűzöm ezt a sort a táblázatom törzséhez
+
+    
+        const sorEsemeny2 = document.createElement(`td`); //létrehozok egy td elemet a második eseménynek ami majd egy új sorba fog kerülni
+        sorEsemeny2.innerHTML = jelenElem.esemeny2;// a cella tartalma az éppen aktuális objektum esemeny2 tulajdonságának értéke lesz
+        sor2.appendChild(sorEsemeny2);//hozzáfűzöm ezt az elemet az újonnan deklarált soromhoz
+    
+        const sorEvszam2  =document.createElement(`td`);//létrehozok egy td elemet a második évszámnak ami majd az új sorba fog kerülni megint
+         sorEvszam2.innerHTML = jelenElem.evszam2;// a cella tartalma az éppen aktuális objektum evszam2 tulajdonságához rendelt érték lesz, ha van az objektumnak ilyen tulajdonsága.
+        sor2.appendChild(sorEvszam2);//hozzáfűzöm ezt az elemet az új soromhoz.
+    }
+
+
+  
+
+
+
+
+    
+
+
+
+
 };
